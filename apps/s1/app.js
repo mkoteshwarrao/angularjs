44aac969890a6b277793282711ee290a8d568879
Koteshwar Rao Myneni <KoteshwarRao.Myneni@hmhco.com>
2016-03-21 14:52:15 +0530

    created DEV branch and added first app s1

---
+++ apps/s1/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("app", [])
+
+.controller('FirstCtrl', function FirstCtrl($scope) {
+   
+    $scope.message = "dynamic content from controller";
+
+    $scope.headder = "Employee List";
+    $scope.employeedata = {"employees":[
+						    {"firstName":"fname1", "lastName":"lname1"},
+						    {"firstName":"fname2", "lastName":"lname2"},
+						    {"firstName":"fname3", "lastName":"lname3"}
+								]};
+
+  })
