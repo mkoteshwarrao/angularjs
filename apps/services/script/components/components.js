9191eb104d95d674bb2b4e32ab0405f0a3a1391b
Koteshwar Rao Myneni <KoteshwarRao.Myneni@hmhco.com>
2016-03-29 14:17:21 +0530

    used angular-strap for $modal implementation

--- apps/services/script/components/components.js
+++ apps/services/script/components/components.js
@@ -18,46 +18,16 @@ app.directive('cFooter', function() {
 
 app.directive('pDetails', function() {
     return {
-      template: '<div class="modal fade">' + 
-          '<div class="modal-dialog">' + 
-            '<div class="modal-content">' + 
-              '<div class="modal-header">' + 
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
-                '<h4 class="modal-title">{{ item.title }}</h4>' + 
-              '</div>' + 
-              '<div class="modal-body" ng-transclude></div>' + 
-               '<h2>Colour : {{item.color}}</h2>'+
-               '<h2>Price : {{ item.price}}</h2>'+
-               '<img ng-src="{{item.image}}" width="100 px" height="100 px" />'+
-               '<div>{{ item.info}}</div>'+
-            '</div>' + 
-          '</div>' + 
-        '</div>',
-      restrict: 'E',
-      transclude: true,
-      replace:true,
-      scope:true,
-      link: function postLink(scope, element, attrs) {
-          scope.$watch(attrs.visible, function(value){
-          if(value == true)
-            $(element).modal('show');
-          else
-            $(element).modal('hide');
-        });
-
-        $(element).on('shown.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = true;
-          });
-        });
-
-        $(element).on('hidden.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = false;
-          });
-        });
-      }
-    };
+      template: '<div>'+
+        '<h2>{{item.title}}</h2>'+
+        '<h2>Colour : {{item.color}}</h2>'+
+        '<h2>Price : {{item.price}}</h2>'+
+        '<img ng-src="{{item.image}}" width="100 px" height="100 px" />'+
+        '<div>{{item.info}}</div>'+
+        '<a ng-href="#/products/">back</a>'+
+      '</div>',
+      restrict: 'E'
+     };
   });
 
  
--- apps/services/script/controllers/productController.js
+++ apps/services/script/controllers/productController.js
@@ -1,35 +1,52 @@
-app.controller('productController', ['$compile', '$scope','$http','productsService',
+app.controller('productController', [ '$scope','$modal','$http','productsService',
 	
-	function productController($compile,$scope,$http,productsService) {
+	function productController( $scope,$modal,$http,productsService) {
 	  $scope.showModal = false;
     $scope.products ={};
-    
+    $scope.myModal = $scope.myModal | {};
     
     $scope.getProducts = productsService.getProducts();
 	
-	 $scope.getProducts.then(
-        // OnSuccess function
+	  $scope.getProducts.then(
+         
         function(items) {
           $scope.products = items.data.products;
           $scope.success = true;
         },
-        // OnFailure function
+        
         function(reason) {
           $scope.somethingWrong = reason;
           $scope.error = true;
         }
-      );
+    );
 
      $scope.showDetails = function(value){
 
    			    angular.forEach($scope.products, function(item){   
- 		                   if(item.id == value){
- 		                   	 $scope.item = item;
- 		                   	 $scope.showModal = !$scope.showModal; 
+ 		                   
+                       if(item.id == value){
+ 		                   	  
+                          $scope.item = item;
+
+                          $scope.myModal = $modal(
+                              {
+                                scope: $scope,
+                                templateUrl: 'pages/pdetails.html',
+                                show: true, 
+                                backdrop: 'static'
+                              }
+                          );
+
+                          $scope.myModal.$promise.then($scope.myModal.show);
+                       
  		                   }
  		            
 		        });
      };
+
+      $scope.hideModal = function(){
+             $scope.myModal.$promise.then($scope.myModal.hide);
+      }
 
 
   }]);
